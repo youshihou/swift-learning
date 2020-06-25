fd5cf6fcd210bb5658ed61820e7c1742f6f26974
oneday <youshihou5@gmail.com>
2020-06-25 23:07:28 +0800

    add collectionView

---
+++ collectionView/collectionView/MountainsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MountainsViewController.swift
+//  collectionView
+//
+//  Created by Ankui on 6/25/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+import UIKit
+
+class MountainsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
