c7a49bf8dd0673abc23fab5012a46bee0873e22f
oneday <youshihou5@gmail.com>
2020-06-26 23:02:20 +0800

    add tableView

---
+++ collectionView/collectionView/WiFiSettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WiFiSettingsViewController.swift
+//  collectionView
+//
+//  Created by Ankui on 6/26/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+import UIKit
+
+class WiFiSettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
