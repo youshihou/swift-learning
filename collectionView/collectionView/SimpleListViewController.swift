d43fa5d77b61c0882c3b67558623bebb8679f157
oneday <youshihou5@gmail.com>
2020-06-27 12:15:51 +0800

    add collectionView

--- collectionView/collectionView/SimpleListViewController.swift
+++ collectionView/collectionView/SimpleListViewController.swift
@@ -8,23 +8,118 @@
 
 import UIKit
 
-class SimpleListViewController: UIViewController {
+class ListCell: UICollectionViewCell {
+    static let identifier = "list-cell-reuse-identifier"
+    
+    let label = UILabel()
+    let accessoryImageView = UIImageView()
+    let seperatorView = UIView()
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        seperatorView.translatesAutoresizingMaskIntoConstraints = false
+        seperatorView.backgroundColor = .lightGray
+        contentView.addSubview(seperatorView)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        contentView.addSubview(label)
+        
+        accessoryImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(accessoryImageView)
+        
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+        
+        let rtl = effectiveUserInterfaceLayoutDirection == .rightToLeft
+        let chevronImageName = rtl ? "chevron.left" : "chevron.right"
+        let chevronImage = UIImage(systemName: chevronImageName)
+        accessoryImageView.image = chevronImage
+        accessoryImageView.tintColor = UIColor.lightGray.withAlphaComponent(0.7)
+        
+        let inset: CGFloat = 10
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+            label.trailingAnchor.constraint(equalTo: accessoryImageView.leadingAnchor, constant: -inset),
+            
+            accessoryImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            accessoryImageView.widthAnchor.constraint(equalToConstant: 13),
+            accessoryImageView.heightAnchor.constraint(equalToConstant: 20),
+            accessoryImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            
+            seperatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            seperatorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            seperatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            seperatorView.heightAnchor.constraint(equalToConstant: 0.5)
+        ])
+        
+    }
+}
 
+
+
+class SimpleListViewController: UIViewController {
+    enum Section {
+        case main
+    }
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "List"
+        
+        configureView()
+        configureDataSource()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
-    */
+    
+    private func configureView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.register(ListCell.self, forCellWithReuseIdentifier: ListCell.identifier)
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
 
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.identifier, for: indexPath) as? ListCell else {
+                fatalError("Cannot create new cell")
+            }
+            cell.label.text = "\(identifier)"
+            return cell
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<99))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension SimpleListViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
