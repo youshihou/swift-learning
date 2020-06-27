a357e7379a392b297d27bb9fd3038cacd5667dea
oneday <youshihou5@gmail.com>
2020-06-27 13:31:12 +0800

    add collectionView

--- collectionView/collectionView/InsetItemsGridViewController.swift
+++ collectionView/collectionView/InsetItemsGridViewController.swift
@@ -9,22 +9,56 @@
 import UIKit
 
 class InsetItemsGridViewController: UIViewController {
+    enum Section {
+        case main
+    }
 
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Inset items Grid"
+        
+        configureView()
+        configureDataSource()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.2), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.2))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
-    */
+    
+    private func configureView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(GridCell.self, forCellWithReuseIdentifier: GridCell.identifier)
+        view.addSubview(collectionView)
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GridCell.identifier, for: indexPath) as? GridCell else {
+                fatalError("Cannot create new cell")
+            }
+            cell.label.text = "\(identifier)"
+            return cell
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<99))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
 
 }
