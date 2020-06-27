1005ecccea63761959623fbbb999c3e8969a4217
oneday <youshihou5@gmail.com>
2020-06-27 20:54:53 +0800

    add collectionView

--- collectionView/collectionView/SectionHeadersFootersViewController.swift
+++ collectionView/collectionView/SectionHeadersFootersViewController.swift
@@ -8,23 +8,123 @@
 
 import UIKit
 
-class SectionHeadersFootersViewController: UIViewController {
+class TitleSupplementaryView: UICollectionReusableView {
+    static let identifier = "title-supplementary-reuse-identifier"
+    
+    let label = UILabel()
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .orange
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 1
+        
+        addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        
+        let inset: CGFloat = 10
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -inset)
+        ])
+    }
+}
 
+
+
+class SectionHeadersFootersViewController: UIViewController {
+    static let sectionHeaderElementKind = "section-header-element-kind"
+    static let sectionFooterElementKind = "section-footer-element-kind"
+    
+    var dataSource: UICollectionViewDiffableDataSource<Int, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Section Headers/Footers"
+
+        configureView()
+        configureDataSource()
+    }
+
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 5
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+        
+        let headerFooterSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerFooterSize, elementKind: SectionHeadersFootersViewController.sectionHeaderElementKind, alignment: .top)
+        let sectionFooter = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerFooterSize, elementKind: SectionHeadersFootersViewController.sectionFooterElementKind, alignment: .bottom)
+        section.boundarySupplementaryItems = [sectionHeader, sectionFooter]
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(ListCell.self, forCellWithReuseIdentifier: ListCell.identifier)
+        collectionView.register(TitleSupplementaryView.self, forSupplementaryViewOfKind: SectionHeadersFootersViewController.sectionHeaderElementKind, withReuseIdentifier: TitleSupplementaryView.identifier)
+        collectionView.register(TitleSupplementaryView.self, forSupplementaryViewOfKind: SectionHeadersFootersViewController.sectionFooterElementKind, withReuseIdentifier: TitleSupplementaryView.identifier)
+        view.addSubview(collectionView)
+        collectionView.delegate = self
     }
-    */
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.identifier, for: indexPath) as? ListCell else {
+                fatalError("Cannot create new cell")
+            }
+            cell.label.text = "\(identifier)"
+            return cell
+        }
+        
+        dataSource.supplementaryViewProvider = {
+            (collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? in
+            guard let headerFooter = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: TitleSupplementaryView.identifier, for: indexPath) as? TitleSupplementaryView else {
+                fatalError("Cannot create new headerView")
+            }
+            
+            let title = "\(kind) for section \(indexPath.section)"
+            headerFooter.label.text = title
+            return headerFooter
+        }
+        
+        let itemPerSection = 5
+        let sections = Array(0..<5)
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        var itemOffset = 0
+        sections.forEach {
+            snapshot.appendSections([$0])
+            snapshot.appendItems(Array(itemOffset..<itemOffset + itemPerSection))
+            itemOffset += itemPerSection
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
 
+
+extension SectionHeadersFootersViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
