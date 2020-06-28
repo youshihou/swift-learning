f829d09389b1c8a2302c1a851e6a58c1ddb3ff63
oneday <youshihou5@gmail.com>
2020-06-28 19:58:26 +0800

    add collectionView

--- collectionView/collectionView/MountainsViewController.swift
+++ collectionView/collectionView/MountainsViewController.swift
@@ -498,7 +498,7 @@ class MountainsViewController: UIViewController {
 
         navigationItem.title = "Mountains Search"
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Nested Groups", style: .plain, target: self, action: #selector(pushNextPage))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Orthogonal Sections", style: .plain, target: self, action: #selector(pushNextPage))
 
         configureView()
         configureDataSource()
@@ -506,7 +506,7 @@ class MountainsViewController: UIViewController {
     }
     
     @objc private func pushNextPage() {
-        let vc = NestedGroupsViewController()
+        let vc = OrthogonalScrollingViewController()
         navigationController?.pushViewController(vc, animated: true)
     }
 
--- collectionView/collectionView/OrthogonalScrollingViewController.swift
+++ collectionView/collectionView/OrthogonalScrollingViewController.swift
@@ -9,22 +9,76 @@
 import UIKit
 
 class OrthogonalScrollingViewController: UIViewController {
-
+    var dataSource: UICollectionViewDiffableDataSource<Int, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Orthogonal Sections"
+        
+        configureView()
+        configureDataSource()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let leadingItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7), heightDimension: .fractionalHeight(1.0)))
+            leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            let trailingItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.3)))
+            trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            
+            let trailingGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1.0)), subitem: trailingItem, count: 2)
+            
+            let containerGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.85), heightDimension: .fractionalHeight(0.4)), subitems: [leadingItem, trailingGroup])
+            
+            let section = NSCollectionLayoutSection(group: containerGroup)
+            section.orthogonalScrollingBehavior = .continuous
+            return section
+        }
+        return layout
     }
-    */
+    
+    private func configureView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(GridCell.self, forCellWithReuseIdentifier: GridCell.identifier)
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GridCell.identifier, for: indexPath) as? GridCell else {
+                fatalError()
+            }
+            cell.label.text = "\(indexPath.section), \(indexPath.item)"
+            cell.contentView.layer.cornerRadius = 8
+            return cell
+        }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        var identifierOffset = 0
+        let itemsPerSection = 30
+        for section in 0..<5 {
+            snapshot.appendSections([section])
+            let maxIdentifier = identifierOffset + itemsPerSection
+            snapshot.appendItems(Array(identifierOffset..<maxIdentifier))
+            identifierOffset += itemsPerSection
+        }
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
 
+}
+
+
+extension OrthogonalScrollingViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
 }
