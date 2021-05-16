7d3a99b3a419f4910a7f01c8c28a742768b3f835
oneday <youshihou5@gmail.com>
2021-05-16 22:05:22 +0800

    update code

--- sources/objectivec-call-swift/objectivec-call-swift/Person.swift
+++ sources/objectivec-call-swift/objectivec-call-swift/Person.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+@objcMembers class Person: NSObject {
+    func test1(v1: Int) {
+        print("test1")
+    }
+    
+    func test2(v1: Int, v2: Int) {
+        print("test2(v1:v2:)")
+    }
+    
+    func test2(_ v1: Double, _ v2: Double) {
+        print("test2(_:_:)")
+    }
+    
+    func run() {
+        perform(#selector(test1))
+        perform(#selector(test1(v1:)))
+        perform(#selector(test2(v1:v2:)))
+        perform(#selector(test2(_:_:)))
+        perform(#selector(test2 as (Double, Double) -> Void))
+    }
+}
